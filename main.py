c3e536b81ceb60cfd8f3cf16539dee0baf61d8a8
Geoffrey Bourdon <geoffreybourdon@gmail.com>
2025-08-02 16:54:00 -0400

    added stats and final game functions

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import random
 import json
 from datetime import datetime
+import pprint
+import time
 
 def import_data() -> dict:
     try:
         with open("rps_data.json") as f:
             data = json.laod(f)
-    except FileExistsError:
+    except FileNotFoundError:
         data = {}
     return data
 
@@ -46,23 +48,45 @@ def initialize_game_list(data: dict, username: str) -> list:
     games = data.get(username, [])
     return games
 
-def main() -> None:
-    """Controls the main logic of the game"""
-    data = import_data()
-    welcome_message()
-    existing_status = check_for_existing_user()
-    username = set_username(existing_status, data)
-    games = initialize_game_list(data, username)
-
-def main_menu() -> str:
+def get_menu_choice() -> str:
     """Takes player input to see if they want to play a round, see the stats or quit the game"""
     while True:
         player_choice = input("Do you want to play, see game stats, or quit (Enter p, s, or q): ").strip().lower()
         if player_choice in ("p", "s", "q"):
             return player_choice
 
-def game_stats()
+def calculate_stats(games: list[dict[str, str]]) -> dict[str, int]:
+    stats = {
+        "Wins": 0,
+        "Losses": 0, 
+        "Ties": 0, 
+        "Rocks thrown": 0,
+        "Papers thrown": 0,
+        "Scissors thrown": 0
+    }
 
+    for game in games:
+        throw = game.get("player choice")
+        if throw == "rock":
+            stats["Rocks thrown"] += 1
+        elif throw == "paper":
+            stats["Papers thrown"] += 1
+        elif throw == "scissors":
+            stats["Scissors thrown"] += 1
+        
+        winner = game.get("winner")
+        if winner == "player":
+            stats["Wins"] += 1
+        elif winner == "computer":
+            stats["Losses"] += 1
+        elif winner == "tie":
+            stats["Ties"] += 1
+    
+    return stats
+
+def show_stats(username, stats: dict[str, int]) -> None:
+    print(f"The stats for {username} are: ")
+    pprint.pprint(stats, sort_dicts = False)
 
 
 def computer_select() -> str:
@@ -98,6 +122,11 @@ def evaluate_winner(player_selection: str, computer_selection: str) -> str:
         return "player"
     return "computer"
 
+def show_winner(winner):
+    print(f"The winner is....\n")
+    time.sleep(.5)
+    print(f"{winner}!!!")
+
 def create_game_dictionary(timestamp: str, player_selection: str, computer_selection: str, winner: str) -> dict:
     """Creates a dictionary with the three crucial pieces of data for the current game"""
     return {
@@ -110,7 +139,34 @@ def create_game_dictionary(timestamp: str, player_selection: str, computer_selec
 def append_game_dictionary(game_dictionary: dict, game_list: list) -> None:
     """Adds the current game data to the list of games for a given user"""
     game_list.append(game_dictionary)
-    
+
+def thank_you_message(username: str) -> None:
+    print(f"Thank you for playing Rock, Paper, Scissors, {username}! Please Play again soon!")
+
+def main() -> None:
+    """Controls the main logic of the game"""
+    data = import_data()
+    welcome_message()
+    existing_status = check_for_existing_user()
+    username = set_username(existing_status, data)
+    games = initialize_game_list(data, username)
+    menu_choice = None
+    while menu_choice != "q":
+        menu_choice = get_menu_choice()
+        if menu_choice == "s":
+            stats = calculate_stats(games)
+            show_stats(username, stats)
+        else:
+            computer_choice = computer_select()
+            player_choice = player_select()
+            timestamp = create_timestamp()
+            winner = evaluate_winner(player_choice, computer_choice)
+            game = create_game_dictionary(timestamp, player_choice, computer_choice, winner)
+            append_game_dictionary(game, games)
+    thank_you_message(username)
+
+if __name__=="__main__":
+    main()
 
 
 
